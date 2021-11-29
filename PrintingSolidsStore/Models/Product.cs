75c19a2f12ae24a9fe8f48af5990bc39c40b254c
Str33tsw33per <zyoungSTEM2019@gmail.com>
2021-11-28 20:31:15 -0500

    v0.1

---
+++ PrintingSolidsStore/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrintingSolidsStore.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product name.")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product price.")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        //public int ProductImage { get; set; }
+
+    }
+}
